910be7bdb91ce083a565a707490c1e180c9427e1
Long Do <longdo@dobtech.vn>
2021-10-09 12:42:19 +0700

    added nats publisher

--- src/routes/new.ts
+++ src/routes/new.ts
@@ -1,7 +1,10 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import { requireAuth, validateRequest } from '@ldxtickets/common';
+
 import { Ticket } from '../models/ticket';
+import { TicketCreatedPublisher } from '../events/publishers/ticket-created-publisher';
+import { natsWrapper } from './../nats-wrapper';
 
 const router = express.Router();
 
@@ -25,6 +28,14 @@ router.post(
       userId: req.currentUser!.id,
     });
     await ticket.save();
+
+    // publish ticket created event to nats streaming to notify other services that are listening
+    await new TicketCreatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+    });
 
     res.status(201).send(ticket);
   }
