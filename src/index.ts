fee0a0298417e3df942bbf21fce4c34dc17d3c22
Long Do <longdo@dobtech.vn>
2021-10-10 14:48:35 +0700

    moved NATS properties into variable env

--- src/index.ts
+++ src/index.ts
@@ -8,14 +8,29 @@ const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
-
   // check for mongo uri
   if (!process.env.MONGO_URI) {
     throw new Error('MONGO_URI must be defined');
   }
+  // check for NATS_CLUSTER_ID
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error('NATS_CLUSTER_ID must be defined');
+  }
+  // check for NATS_CLIENT_ID
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error('NATS_CLIENT_ID must be defined');
+  }
+  // check for NATS_URL
+  if (!process.env.NATS_URL) {
+    throw new Error('NATS_URL must be defined');
+  }
 
   try {
-    await natsWrapper.connect('ticketing', 'asdasda', 'http://nats-srv:4222');
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
     natsWrapper.client.on('close', () => {
       console.log('NATS connection closed!');
       process.exit();
